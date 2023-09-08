a350abc709c1def77b26f10fd056f993045a20ec
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-07 22:07:12 -0500

    Made DisplayGrid scroll vertically

--- src/ItemDisplay/DisplayGrid.tsx
+++ src/ItemDisplay/DisplayGrid.tsx
@@ -5,7 +5,7 @@ import RetracableDisplay from "./Items/RetractableDisplay/RetractableDisplay";
 import SpinDisplay from "./Items/SpinDisplay/SpinDisplay";
 
 const DisplayGrid = () => {
-    return (<div className="grid grid-cols-2 gap-4 p-4">
+    return (<div className="grid grid-cols-2 gap-4 p-4 overflow-y-auto">
         <BasicDisplayOne />
         <BasicDisplayTwo />
         <BasicDisplayThree />
