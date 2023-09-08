5c77a1472ea10622c863e7380b83ac8f60cf0081
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-07 22:07:40 -0500

    Updated BasicDisplay with template data

--- src/ItemDisplay/Items/BasicDisplay/BasicDisplayThree.tsx
+++ src/ItemDisplay/Items/BasicDisplay/BasicDisplayThree.tsx
@@ -2,6 +2,19 @@ import tape from "../../../assets/ItemImages/tape.webp";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { IconContext } from "react-icons";
 import { useState } from "react";
+
+const sampleItemData = {
+  name: "Purple Bunny",
+  id: "washi_tape_purple_bunny",
+  price: "6.99",
+  currency: "USD",
+  product: "tape",
+  onSale: true,
+  salePrice: "3.99",
+  inStock: true,
+  qtyInStock: 99
+};
+
 const BasicDisplayThree = () => {
   const [favorite, setFavorite] = useState(false);
   const updateFavorite = () => {
@@ -12,9 +25,21 @@ const BasicDisplayThree = () => {
   return (
     <div className="flex flex-col">
       <div className="text-xl text-blue-600 rounded-t-full self-center h-14 w-full bg-gradient-to-b from-indigo-300 via-purple-300 to-pink-300">
-        <p className="text-center mt-2 mb-0">Purple Bunny</p>
-        <p className=" text-red-600 text-sm text-center">
-          $3.99 <span className="line-through text-gray-500">$6.99</span>
+        <p className="text-center mt-2 mb-0">{sampleItemData.name}</p>
+        <p className="text-sm text-center">
+          {sampleItemData.currency}{" "}
+          {sampleItemData.onSale && (
+            <span className="text-red-600 text-sm">
+              ${sampleItemData.salePrice}
+            </span>
+          )}{" "}
+          <span
+            className={`${
+              sampleItemData.onSale ? "line-through" : ""
+            } text-gray-500`}
+          >
+            ${sampleItemData.price}
+          </span>
         </p>
       </div>
       <img src={tape} />
