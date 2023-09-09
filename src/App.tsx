aac7e47f457fafb713511776485b4c3b0e76b012
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-08 20:08:53 -0500

    Added NavigationBar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import DisplayGrid from "./ItemDisplay/DisplayGrid"
+import DisplayGrid from "./Components/ItemDisplay/DisplayGrid";
+import NavigationBar from "./Components/Navigation/NavigationBar";
 
 function App() {
-
   return (
-    <DisplayGrid />
-  )
+    <>
+      <NavigationBar />
+      <DisplayGrid />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Navigation/NavigationBar.tsx
@@ -0,0 +1,5 @@
+const NavigationBar = () => {
+    return <div className="h-12 w-max bg-slate-500">Navigation Bar</div>
+}
+
+export default NavigationBar;
