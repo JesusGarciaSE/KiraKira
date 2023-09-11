6c3f3be4b7502a1aa1915754c252a01e804de2bd
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-11 17:55:48 -0500

    Updated Top Navigation Bar

--- src/Components/Navigation/NavigationBar.tsx
+++ src/Components/Navigation/NavigationBar.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineAccountCircle } from "react-icons/md";
 import NavigationSideBar from "./NavigationSideBar";
+import logo from "../../assets/SiteImages/Kirakira_logo_placeholder_line.png";
 
 const NavigationBar = () => {
   const [active, setActive] = useState(false);
 
   const toggleMenu = (): void => {
     setActive((prevActive) => {
-    if (!prevActive) {
+      if (!prevActive) {
         document.body.classList.add("overflow-hidden");
       } else {
         document.body.classList.remove("overflow-hidden");
@@ -16,13 +18,20 @@ const NavigationBar = () => {
     });
   };
   return (
-    <div className=" flex flex-row justify-evenly bg-slate-500">
-      <div className="p-3" onClick={toggleMenu}>
-        <GiHamburgerMenu className="text-blue-300 h-12 w-12" />
+    <div className=" flex flex-row justify-around bg-gradient-to-br from-indigo-300 via-purple-300 to-pink-300">
+      <div className="pl-3 my-auto" onClick={toggleMenu}>
+        <GiHamburgerMenu className="text-blue-600 h-12 w-12" />
       </div>
-      <p className="m-auto">Title</p>
-      <div className="my-auto">
-        <p>end</p>
+      <div className="w-48 mx-auto relative">
+        <img
+          className="max-h-full max-w-full"
+          src={logo}
+          alt="Linear alternate logo"
+        />
+        <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl font-custom text-transparent bg-clip-text bg-clip bg-gradient-to-br from-indigo-700 via-purple-700 to-pink-700">KiraKira</p>
+      </div>
+      <div className="pr-3 my-auto" onClick={toggleMenu}>
+        <MdOutlineAccountCircle className="text-blue-600 h-12 w-12" />
       </div>
       <NavigationSideBar
         className="absolute top-0 left-0 z-20 bg-red-300"
