dd4386c9465dfd35ef9ddc684e50312abfd0df22
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-11 12:48:19 -0500

    Added KiraKira logo placeholder

--- src/Components/Navigation/NavigationSideBar.tsx
+++ src/Components/Navigation/NavigationSideBar.tsx
@@ -1,5 +1,6 @@
 import { GiNinjaStar } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import logo from "../../assets/SiteImages/Kirakira_logo_placeholder.png";
 
 interface INavigationSideBar {
   className?: string;
@@ -23,8 +24,9 @@ const NavigationSideBar: React.FC<INavigationSideBar> = ({
     >
       <div className="flex flex-row">
         <GiNinjaStar className="text-red-400 h-12 w-12" onClick={onClick} />
-        <div className="grid content-center flex-1">
-          <p>KiraKira</p>
+        <div className="grid content-center flex-1 relative">
+          <img loading="lazy" src={logo} alt="Logo"/>
+          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl font-custom text-transparent bg-clip-text bg-clip bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600">KiraKira</p>
         </div>
       </div>
       <ul className="flex-grow">
