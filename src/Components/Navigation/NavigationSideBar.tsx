a213c6f831f85c6889263439fec06880ec141e18
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-11 18:55:58 -0500

    Restyled the sidebar logo

--- src/Components/Navigation/NavigationSideBar.tsx
+++ src/Components/Navigation/NavigationSideBar.tsx
@@ -20,21 +20,33 @@ const NavigationSideBar: React.FC<INavigationSideBar> = ({
 }) => {
   return (
     <div
-      className={`flex flex-col min-h-screen min-w-full p-3 text-2xl text-center ease-in-out duration-300 ${className} ${color} ${active ? 'translate-x-0' : '-translate-x-full'}`}
+      className={`flex flex-col min-h-screen min-w-full p-3 text-2xl text-center ease-in-out duration-300 ${className} ${color} ${
+        active ? "translate-x-0" : "-translate-x-full"
+      }`}
     >
-      <div className="flex flex-row">
-        <GiNinjaStar className="text-red-400 h-12 w-12" onClick={onClick} />
+      <div className="relative">
+        <GiNinjaStar
+          className="absolute z-10 text-red-400 h-12 w-12"
+          onClick={onClick}
+        />
         <div className="grid content-center flex-1 relative">
-          <img loading="lazy" src={logo} alt="Logo"/>
-          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl font-custom text-transparent bg-clip-text bg-clip bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600">KiraKira</p>
+          <div className="h-48 mx-auto">
+            <img
+              loading="lazy"
+              className="max-h-full max-w-full"
+              src={logo}
+              alt="Logo"
+            />
+          </div>
+          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl font-custom text-transparent bg-clip-text bg-clip bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600">
+            KiraKira
+          </p>
         </div>
       </div>
       <ul className="flex-grow">
         {pages.map((page, index) => (
           <Link to={page.toLowerCase()} key={`${index}_${page}`}>
-            <li className="py-3">
-              {page}
-            </li>
+            <li className="py-3">{page}</li>
           </Link>
         ))}
       </ul>
