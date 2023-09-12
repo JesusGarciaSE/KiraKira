80a3ba121702bf20556a12a5474ad48e07a2b9c3
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-11 20:33:16 -0500

    Added UserNavigation to mirror the main navigation menu

--- src/Components/Navigation/NavigationBar.tsx
+++ src/Components/Navigation/NavigationBar.tsx
@@ -3,20 +3,23 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineAccountCircle } from "react-icons/md";
 import NavigationSidebar from "./NavigationSideBar";
 import logo from "../../assets/SiteImages/Kirakira_logo_placeholder_line.png";
+import UserNavigation from "./UserNavigation";
 
 const NavigationBar = () => {
   const [active, setActive] = useState(false);
+  const [userNavActive, setUserNavActive] = useState(false);
 
   const toggleMenu = (): void => {
     setActive((prevActive) => {
-      if (!prevActive) {
-        document.body.classList.add("overflow-hidden");
-      } else {
-        document.body.classList.remove("overflow-hidden");
-      }
       return !prevActive;
     });
   };
+  const toggleUserMenu = ():void => {
+    setUserNavActive((prevUserNavActive) => {
+      return !prevUserNavActive;
+    })
+  }
+
   return (
     <div className=" flex flex-row justify-around bg-gradient-to-br from-indigo-300 via-purple-300 to-pink-300">
       <div className="pl-3 my-auto" onClick={toggleMenu}>
@@ -30,7 +33,7 @@ const NavigationBar = () => {
         />
         <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl font-custom text-transparent bg-clip-text bg-clip bg-gradient-to-br from-indigo-700 via-purple-700 to-pink-700">KiraKira</p>
       </div>
-      <div className="pr-3 my-auto" onClick={toggleMenu}>
+      <div className="pr-3 my-auto" onClick={toggleUserMenu}>
         <MdOutlineAccountCircle className="text-blue-600 h-12 w-12" />
       </div>
       <NavigationSidebar
@@ -38,6 +41,12 @@ const NavigationBar = () => {
         color="bg-gradient-to-br from-indigo-300 via-purple-300 to-pink-300"
         active={active}
         onClick={toggleMenu}
+      />
+      <UserNavigation
+        className="absolute top-0 left-0 z-20 bg-red-300"
+        color="bg-gradient-to-br from-indigo-300 via-purple-300 to-pink-300"
+        active={userNavActive}
+        onClick={toggleUserMenu}
       />
     </div>
   );
---
+++ src/Components/Navigation/UserNavigation.tsx
@@ -0,0 +1,58 @@
+import { GiNinjaStar } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import logo from "../../assets/SiteImages/Kirakira_logo_placeholder.png";
+
+interface INavigationSidebar {
+  className?: string;
+  active?: boolean;
+  size?: string;
+  color?: string;
+  onClick(): void;
+}
+
+const pages = ["Account", "Cart", "Settings", "Logout"];
+
+const UserNavigation: React.FC<INavigationSidebar> = ({
+  className,
+  active,
+  color,
+  onClick,
+}) => {
+  return (
+    <div
+      className={`flex flex-col min-h-screen min-w-full p-3 text-2xl text-center ease-in-out duration-300 ${className} ${color} ${
+        active ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="relative">
+        <GiNinjaStar
+          className={`absolute z-10 right-1 text-red-400 h-12 w-12`}
+          onClick={onClick}
+        />
+        <div className="grid content-center flex-1 relative">
+          <div className="h-48 mx-auto">
+            <img
+              loading="lazy"
+              className="max-h-full max-w-full"
+              src={logo}
+              alt="Logo"
+            />
+          </div>
+          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl font-custom text-transparent bg-clip-text bg-clip bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-600">
+            KiraKira
+          </p>
+        </div>
+      </div>
+      <ul className="flex-grow">
+        {pages.map((page, index) => (
+          <Link to={page.toLowerCase()} key={`${index}_${page}`}>
+            <li className="py-3">{page}</li>
+          </Link>
+        ))}
+      </ul>
+      <p className="text-sm">KiraKira, LLC 2023</p>
+    </div>
+  );
+};
+
+export default UserNavigation;
