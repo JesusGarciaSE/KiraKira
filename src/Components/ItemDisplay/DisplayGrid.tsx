7c346804219e0a6b4bf4582223dd98d17e755531
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-09-14 15:45:57 -0500

    Updated DisplayGrid to use data pulled from Firestore

--- src/Components/ItemDisplay/DisplayGrid.tsx
+++ src/Components/ItemDisplay/DisplayGrid.tsx
@@ -1,144 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemDetailsModal from "./ItemDisplayModal/ItemDetailsModal";
 import BasicDisplaySix from "./Items/BasicDisplay/BasicDisplaySix";
 import RetracableDisplay from "./Items/RetractableDisplay/RetractableDisplay";
 import SpinDisplay from "./Items/SpinDisplay/SpinDisplay";
 import { IItem } from "../../Models/ItemModels";
 import ItemDetails from "./ItemDisplayModal/ItemDetails";
-import tape from "../../assets/ItemImages/tape.webp";
+import { firestore } from "../../Services/FirebaseServices";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
-const sampleItemData = [
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-  {
-    name: "Purple Bunny",
-    image: tape,
-    id: "washi_tape_purple_bunny",
-    description:
-      "A basic purple washi tape. You can stick it on things and it will make them cuter!",
-    price: "6.99",
-    currency: "USD",
-    product: "tape",
-    onSale: true,
-    salePrice: "3.99",
-    inStock: true,
-    qtyInStock: 99,
-  },
-];
 
-const DisplayGrid = () => {
+interface IDisplayGrid {
+  category: string;
+}
+
+const DisplayGrid: React.FC<IDisplayGrid> = ({ category }) => {
   const [showModal, setShowModal] = useState(false);
   const [selectedItem, setSelectedItem] = useState<IItem | null>(null);
+  const [productList, setProductList] = useState<IItem[]>([]);
   const showItemModal = (item: IItem) => {
     setSelectedItem(item);
     setShowModal(true);
@@ -147,12 +25,31 @@ const DisplayGrid = () => {
     setSelectedItem(null); // Clear the selected item
     setShowModal(false);
   };
+
+  useEffect(() => {
+    const queryProducts = query(
+      collection(firestore, "Products"),
+      where("category", "==", "correction tape")
+    );
+    const products: IItem[] = [];
+    const querySnapshot = getDocs(queryProducts);
+    querySnapshot.then((response) => {
+      response.docs.forEach((doc) => {
+        console.log(doc.data())
+        if (doc.data) {
+          products.push(doc.data() as IItem);
+        }
+      });
+
+      setProductList(products);
+    });
+  }, [category]);
   return (
     <div className="grid grid-cols-2 gap-4 p-4 overflow-y-auto">
       <ItemDetailsModal isVisible={showModal} onClose={closeItemModal}>
         {selectedItem && <ItemDetails item={selectedItem} />}
       </ItemDetailsModal>
-      {sampleItemData.map((item, index) => (
+      {productList.map((item, index) => (
         <BasicDisplaySix
           key={`${item.id}_${index}`}
           item={item}
