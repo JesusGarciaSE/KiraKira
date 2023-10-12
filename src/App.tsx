5e002bada598cf49dd5b10b75f73fa86da32334f
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-10-12 02:59:12 -0500

    Added new catalogue context
    Catalogue context will hold all product information and be able to distribute it throughout the app as needed.
    A single document call to query all products should save on reads and write in the long run.
    This may cause issues however if our catalogue get's too large.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Outlet } from "react-router-dom";
 import NavigationBar from "./Components/Navigation/NavigationBar";
 import AuthContextProvider from "./Services/AuthContext";
 import CartContextProvider from "./Services/CartContext";
+import CatalogueContextProvider from "./Services/CatalogueContext";
 
 function App() {
   return (
     <div className="flex flex-col h-screen bg-gradient-to-br from-kira-bg-start via-kira-bg-through to-kira-bg-end">
       <AuthContextProvider>
-        <CartContextProvider>
-          <NavigationBar />
-          <Outlet />
-        </CartContextProvider>
+        <CatalogueContextProvider>
+          <CartContextProvider>
+            <NavigationBar />
+            <Outlet />
+          </CartContextProvider>
+        </CatalogueContextProvider>
       </AuthContextProvider>
     </div>
   );
