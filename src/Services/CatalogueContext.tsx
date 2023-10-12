59b4382a152f1a42c381a1f00d881aad80e5453d
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-10-12 03:20:29 -0500

    Adjusted CatalogueContext interface

--- src/Services/CatalogueContext.tsx
+++ src/Services/CatalogueContext.tsx
@@ -5,14 +5,16 @@ import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "./FirebaseServices";
 
 interface Catalogue {
-  catalogue: {
-    Bookmarks: IItem[];
-    Magnets: IItem[];
-    Stickers: IItem[];
-  };
+  Bookmarks: IItem[];
+  Magnets: IItem[];
+  Stickers: IItem[];
 }
 
-export const CatalogueContext = createContext<Catalogue>({
+interface CatalogueContext {
+  catalogue: Catalogue;
+}
+
+export const CatalogueContext = createContext<CatalogueContext>({
   catalogue: {
     Bookmarks: [],
     Magnets: [],
@@ -20,14 +22,14 @@ export const CatalogueContext = createContext<Catalogue>({
   },
 });
 
-export const useCatalogue = (): Catalogue => {
+export const useCatalogue = (): CatalogueContext => {
   return useContext(CatalogueContext);
 };
 
 export const CatalogueContextProvider: React.FC<IParentComponent> = ({
   children,
 }) => {
-  const [catalogue, setCatalogue] = useState<Catalogue>({
+  const [catalogue, setCatalogue] = useState<CatalogueContext>({
     catalogue: {
       Bookmarks: [],
       Magnets: [],
@@ -39,7 +41,7 @@ export const CatalogueContextProvider: React.FC<IParentComponent> = ({
       const collectionsDocRef = doc(firestore, "Products", "Collections");
       const collectionsDoc = await getDoc(collectionsDocRef);
       collectionsDoc.exists()
-        ? setCatalogue(collectionsDoc.data() as Catalogue)
+        ? setCatalogue({ catalogue: collectionsDoc.data() as Catalogue })
         : console.log("Error getting catalogue");
     };
     getCatalogue();
